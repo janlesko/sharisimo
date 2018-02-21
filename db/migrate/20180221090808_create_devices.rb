9de1c7e97de0df4f13f0d6899ad30b8adf1e3052
Jan Lesko <jan.lesko.sk@gmail.com>
2018-02-21 17:10:09 +0800

    create models

---
+++ db/migrate/20180221090808_create_devices.rb
@@ -0,0 +1,14 @@
+class CreateDevices < ActiveRecord::Migration[5.1]
+  def change
+    create_table :devices do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :photo_url
+      t.string :description
+      t.boolean :availability
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
